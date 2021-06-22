db46adacf7a6994b9d7a51c9ac652a3f22a72b41
Serge Chastel <schastel@hawaii.edu>
2021-06-21 14:13:36 -1000

    Comments moved to readme

--- build.sh
+++ build.sh
@@ -6,23 +6,3 @@ docker build -t docker.io/schastel/anfy:latest .
 # Push image to docker.io (need account)
 # docker push docker.io/schastel/anfy:latest
 
-#
-#
-#  U S A G E
-#
-#
-# Pull image from docker.io (no account needed)
-# docker pull docker.io/schastel/anfy:latest
-#
-# Run a container:
-# docker run --rm -ti docker.io/schastel/anfy:latest bash
-# Notes:
-# --rm: Delete the container when exiting
-# -ti: Interactive mode (equivalent to -t -i) 
-#
-# SUGGESTIONS:
-# - Use a (local) docker volume to store the index files
-# -> Save space 
-# - Use a (local) docker volume for the work directory
-# -> Save your work outside docker 
-#
